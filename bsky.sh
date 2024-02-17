50ec61df02583d250e932ec4a42f37379c6d9f4c
taikii <taikii.git@outlook.com>
2024-02-17 13:34:17 +0900

    Update usage

--- bsky.sh
+++ bsky.sh
@@ -323,7 +323,41 @@ function _list_feed() {
 # _usage
 ########
 function _usage() {
-	cat "$(dirname "$(readlink -f "$0")")/README.md"
+	cat <<- EOS
+		./bsky.sh login
+
+		./bsky.sh refresh-session
+
+		./bsky.sh profile [HANDLE]
+			did handle
+
+		./bsky.sh follows [HANDLE]
+			did handle
+
+		./bsky.sh followers [HANDLE]
+			did handle
+
+		./bsky.sh lists [HANDLE]
+				uri collection name
+
+		./bsky.sh list LIST_URI
+				rkey did handle
+
+		./bsky.sh addmember LIST_URI USER_DID
+		USER_DIDs | ./bsky.sh addmember LIST_URI
+
+		./bsky.sh delmember LIST_URI USER_DID
+		USER_DIDs | ./bsky.sh delmember LIST_URI
+
+		./bsky.sh delmember-rkey LIST_MEMBER_RKEY 
+		LIST_MEMBER_RKEYs | ./bsky.sh delmember_rkey
+
+		./bsky.sh feed FEED_URI
+			uri createdAt handle text
+
+		./bsky.sh follows [HANDLE]
+			uri createdAt handle text
+	EOS
 }
 
 if [[ $# -eq 0 ]]; then
